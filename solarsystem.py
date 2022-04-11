c80daa7e75bec65b4568139ccd46f12c1a10dfb8
Jack Whitworth <45968087+Jack-Writes-Code@users.noreply.github.com>
2022-04-11 10:34:07 +0100

    Update solarsystem.py

--- solarsystem.py
+++ solarsystem.py
@@ -22,7 +22,7 @@ class star():
     def generate(self, WIN):
         pg.draw.circle(WIN, self.colour, (self.x, self.y), self.size*10)
 
-class satelite():
+class satellite():
     instances = []
     def __init__(self, parent, size, distance, velocity, colour):
         self.__class__.instances.append(self)
@@ -50,37 +50,37 @@ class satelite():
 
 sun = star(5, WIDTH/2, HEIGHT/2, (255,255,0))
 #Planets
-mercury = satelite(sun, 1, 1, -48, (219,206,202))
-venus = satelite(sun, 3, 2, 35, (150,131,150))
-earth = satelite(sun, 4, 3, 30, (0,0,205))
-mars = satelite(sun, 2, 4, -24, (193,68,14))
-jupiter = satelite(sun, 8, 6, 13, (227,110,75))
-saturn = satelite(sun, 7, 7, -10, (206,184,184))
-uranus = satelite(sun, 6, 8, -7, (213,251,252))
-neptune = satelite(sun, 5, 9, 5, (91,93,223))
+mercury = satellite(sun, 1, 1, -48, (219,206,202))
+venus = satellite(sun, 3, 2, 35, (150,131,150))
+earth = satellite(sun, 4, 3, 30, (0,0,205))
+mars = satellite(sun, 2, 4, -24, (193,68,14))
+jupiter = satellite(sun, 8, 6, 13, (227,110,75))
+saturn = satellite(sun, 7, 7, -10, (206,184,184))
+uranus = satellite(sun, 6, 8, -7, (213,251,252))
+neptune = satellite(sun, 5, 9, 5, (91,93,223))
 #Moons
-mercury_moon = satelite(mercury, 0.6, 0.1, 200, (150,131,150))
-venus_moon = satelite(venus, 0.8, 0.3, -180, (150,131,150))
-venus_moon = satelite(venus, 1, 0.4, 200, (150,131,150))
-earth_moon = satelite(earth, 0, 0.2, -300, (0,0,0))
-mars_moon = satelite(mars, 0.4, 0.25, -380, (150,131,150))
-mars_moon = satelite(mars, 0.7, 0.4, 240, (150,131,150))
-jupiter_moon = satelite(jupiter, 2, 0.6, 200, (150,131,150))
-jupiter_moon = satelite(jupiter, 1.4, 0.45, -380, (150,131,150))
-jupiter_moon = satelite(jupiter, 0.7, 0.6, 240, (150,131,150))
-saturn_moon = satelite(saturn, 2, 0.6, 200, (150,131,150))
-saturn_moon = satelite(saturn, 1.4, 0.45, -380, (150,131,150))
-uranus_moon = satelite(uranus, 2, 0.6, 200, (150,131,150))
-uranus_moon = satelite(uranus, 1.4, 0.45, -380, (150,131,150))
-uranus_moon = satelite(uranus, 0.7, 0.6, 240, (150,131,150))
-neptune_moon = satelite(neptune, 1.4, 0.45, -380, (150,131,150))
-neptune_moon = satelite(neptune, 0.7, 0.6, 240, (150,131,150))
+mercury_moon = satellite(mercury, 0.6, 0.1, 200, (150,131,150))
+venus_moon = satellite(venus, 0.8, 0.3, -180, (150,131,150))
+venus_moon = satellite(venus, 1, 0.4, 200, (150,131,150))
+earth_moon = satellite(earth, 0, 0.2, -300, (0,0,0))
+mars_moon = satellite(mars, 0.4, 0.25, -380, (150,131,150))
+mars_moon = satellite(mars, 0.7, 0.4, 240, (150,131,150))
+jupiter_moon = satellite(jupiter, 2, 0.6, 200, (150,131,150))
+jupiter_moon = satellite(jupiter, 1.4, 0.45, -380, (150,131,150))
+jupiter_moon = satellite(jupiter, 0.7, 0.6, 240, (150,131,150))
+saturn_moon = satellite(saturn, 2, 0.6, 200, (150,131,150))
+saturn_moon = satellite(saturn, 1.4, 0.45, -380, (150,131,150))
+uranus_moon = satellite(uranus, 2, 0.6, 200, (150,131,150))
+uranus_moon = satellite(uranus, 1.4, 0.45, -380, (150,131,150))
+uranus_moon = satellite(uranus, 0.7, 0.6, 240, (150,131,150))
+neptune_moon = satellite(neptune, 1.4, 0.45, -380, (150,131,150))
+neptune_moon = satellite(neptune, 0.7, 0.6, 240, (150,131,150))
 #Background Stars
 for i in range(0,200):
     bg_stars = star(uniform(1.0,2.0)/10, randrange(0,WIDTH), randrange(0,HEIGHT), (randrange(220,255),randrange(220,255),randrange(220,255)))
 #Asteroid Belt
 for i in range(0,2000):
-    asteroid = satelite(sun, uniform(0.1,1.0), uniform(4.5, 5.2), randrange(20,22), (219,206,202))
+    asteroid = satellite(sun, uniform(0.1,1.0), uniform(4.5, 5.2), randrange(20,22), (219,206,202))
 
 
 if __name__ == '__main__':
@@ -91,7 +91,7 @@ if __name__ == '__main__':
 
         WIN.fill((0, 0, 0)) #Draw background
         [star.generate(WIN) for star in star.instances] #Draw stars
-        [satelite.calculateMovement(WIN) for satelite in satelite.instances] #Draw satelites
+        [satellite.calculateMovement(WIN) for satellite in satellite.instances] #Draw satellites
         pg.display.update() #Update screen
 
         for event in pg.event.get():
