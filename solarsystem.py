40d56085ede7c7adfb9497c98c8d9cbdf07cdce3
Jack Whitworth <45968087+Jack-Writes-Code@users.noreply.github.com>
2022-04-11 09:49:26 +0100

    Restructured the script to be more readable

--- solarsystem.py
+++ solarsystem.py
@@ -1,29 +1,14 @@
+from math           import sin, cos, pi, radians
+from random         import randrange, uniform
+import pygame       as pg
 import sys
-import pygame as pg
-from math import sin,cos,pi, radians
-from random import randrange, uniform
 
-WIDTH, HEIGHT = 1600, 900 #Set size of windpw
+
+WIDTH, HEIGHT = 1600, 900
 WIN = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Solar System")
+FPS = 60
 
-pg.display.set_caption("Solar System") #Name window
-
-FPS = 60 #Set desired FPS for clock
-
-def main(stars, satelites):
-    clock = pg.time.Clock()
-    run = True
-    while run: # test if program has been quit
-        clock.tick(FPS)
-
-        WIN.fill((0, 0, 0)) #Draw background
-        [star.generate(WIN) for star in stars] #Draw stars
-        [satelite.calculateMovement(WIN) for satelite in satelites] #Draw satelites
-        pg.display.update() #Update screen
-
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                run = False
 
 class star():
     instances = []
@@ -62,15 +47,9 @@ class satelite():
         self.x = self.x + self.radius * self.velocity * cos(self.angle + pi / 2) # New x
         self.y = self.y - self.radius * self.velocity * sin(self.angle + pi / 2) # New y
 
-# stars(size, x, y, colour)
-    #background stars
-for i in range(0,200):
-    bg_stars = star(uniform(1.0,2.0)/10, randrange(0,WIDTH), randrange(0,HEIGHT), (randrange(220,255),randrange(220,255),randrange(220,255)))
 
-    #the Sun
 sun = star(5, WIDTH/2, HEIGHT/2, (255,255,0))
-
-# planet satelites(orbitining, size, distance, velocity, colour)
+#Planets
 mercury = satelite(sun, 1, 1, -48, (219,206,202))
 venus = satelite(sun, 3, 2, 35, (150,131,150))
 earth = satelite(sun, 4, 3, 30, (0,0,205))
@@ -79,40 +58,50 @@ jupiter = satelite(sun, 8, 6, 13, (227,110,75))
 saturn = satelite(sun, 7, 7, -10, (206,184,184))
 uranus = satelite(sun, 6, 8, -7, (213,251,252))
 neptune = satelite(sun, 5, 9, 5, (91,93,223))
-
-# moon satelites(orbitining, size, distance, velocity, colour)
-    #mercury
-venus_moon = satelite(mercury, 0.6, 0.1, 200, (150,131,150))
-    #venus
+#Moons
+mercury_moon = satelite(mercury, 0.6, 0.1, 200, (150,131,150))
 venus_moon = satelite(venus, 0.8, 0.3, -180, (150,131,150))
 venus_moon = satelite(venus, 1, 0.4, 200, (150,131,150))
-    #earth
-Tmoon = satelite(earth, 0, 0.2, -300, (0,0,0))
-Ttmoon = satelite(Tmoon, 0, 0.2, 350, (0,0,0))
-Tttmoon = satelite(Ttmoon, 1, 0.2, -250, (255,255,255))
-    #mars
+earth_moon = satelite(earth, 0, 0.2, -300, (0,0,0))
 mars_moon = satelite(mars, 0.4, 0.25, -380, (150,131,150))
 mars_moon = satelite(mars, 0.7, 0.4, 240, (150,131,150))
-    #jupiter
 jupiter_moon = satelite(jupiter, 2, 0.6, 200, (150,131,150))
 jupiter_moon = satelite(jupiter, 1.4, 0.45, -380, (150,131,150))
 jupiter_moon = satelite(jupiter, 0.7, 0.6, 240, (150,131,150))
-    #saturn
 saturn_moon = satelite(saturn, 2, 0.6, 200, (150,131,150))
 saturn_moon = satelite(saturn, 1.4, 0.45, -380, (150,131,150))
-    #uranus
 uranus_moon = satelite(uranus, 2, 0.6, 200, (150,131,150))
 uranus_moon = satelite(uranus, 1.4, 0.45, -380, (150,131,150))
 uranus_moon = satelite(uranus, 0.7, 0.6, 240, (150,131,150))
-    #neptune
 neptune_moon = satelite(neptune, 1.4, 0.45, -380, (150,131,150))
 neptune_moon = satelite(neptune, 0.7, 0.6, 240, (150,131,150))
-
-#asteroid belt - MAKE NEW CLASS
+#Background Stars
+for i in range(0,200):
+    bg_stars = star(uniform(1.0,2.0)/10, randrange(0,WIDTH), randrange(0,HEIGHT), (randrange(220,255),randrange(220,255),randrange(220,255)))
+#Asteroid Belt
 for i in range(0,2000):
     asteroid = satelite(sun, uniform(0.1,1.0), uniform(4.5, 5.2), randrange(20,22), (219,206,202))
 
+
 if __name__ == '__main__':
-    main(star.instances, satelite.instances)
+    
+    
+    
+    
+    
+    
+    
+    clock = pg.time.Clock()
+    run = True
+    while run: # test if program has been quit
+        clock.tick(FPS)
+
+        WIN.fill((0, 0, 0)) #Draw background
+        [star.generate(WIN) for star in star.instances] #Draw stars
+        [satelite.calculateMovement(WIN) for satelite in satelite.instances] #Draw satelites
+        pg.display.update() #Update screen
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
     quit()
-    sys.exit()
