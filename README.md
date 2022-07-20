34ad3d879b5415a27c5e8cf03b8bc7bcd91f5941
Jack Whitworth <45968087+Jack-Writes-Code@users.noreply.github.com>
2022-07-20 11:30:28 +0100

    zooming and clickdrag camera working

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # solar_system
-My first pygame project for practice using classes. Simulates the solar system.
 
-
-Written in Python, this was an exercise of using classes, inheritance, and algebra within programs.
-
-This is fully object orientated and you can see it in action here: https://youtu.be/ohw6_cHmHGE
+All sizes/distances are in meters, and velocity is in miless per hour (MPH)
--- solarsystem.py
+++ solarsystem.py
@@ -15,106 +15,177 @@ FPS = SETTINGS["Screen"]["FPS"]
 WIN = pg.display.set_mode((SETTINGS["Screen"]["Width"], SETTINGS["Screen"]["Height"]))
 pg.display.set_caption(SETTINGS["Screen"]["Title"])
 
-#SPRITE GROUP FOR UPDATING AND SEARCHING
-visible_sprites = pg.sprite.Group()
 
-
-def get_body_by_name(name):
+class MoveableCameraGroup(pg.sprite.Group):
     """
-    SEARCHES ALL SPRITES IN THE VISIBLE SPRITE GROUP
-    AND RETURNS THE SPRITE WITH THE GIVEN NAME
+    SPRITE GROUP SUBCLASS TO HANDLE MOUSE INPUTS AS OFFSETS FOR A 'CAMERA'
     """
-    for sprite in visible_sprites:
-        if sprite.name == name:
-            return sprite
-    return False
+    def __init__(self, SETTINGS):
+        super().__init__()
+        self.scale_size     = SETTINGS["Scales"]["Size"]
+        self.scale_velocity = SETTINGS["Scales"]["Velocity"]
+        self.scale_distance = SETTINGS["Scales"]["Distance"]
+        self.incriment_up   = SETTINGS["Scales"]["Incriments"]["Up"]
+        self.incriment_down = SETTINGS["Scales"]["Incriments"]["Down"]
+        
+        self.offset             = pg.math.Vector2() #TO APPLY TO SPRITES
+        self.clickstart_offset  = pg.math.Vector2() #NORMALISE AFTER CLICK
+        self.dragging           = False #WHEN TO APPLY OFFSET TO SPRITES
+    
+    def move_camera(self, event):
+        """
+        CLICK AND DRAG TO MOVE THE 'CAMERA'
+        """
+        if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+            self.dragging = True
+            mouse_x, mouse_y = pg.mouse.get_pos()
+            self.clickstart_offset.x = self.offset.x - mouse_x
+            self.clickstart_offset.y = self.offset.y - mouse_y
+        
+        elif event.type == pg.MOUSEBUTTONUP and event.button == 1:
+            self.dragging = False
+        
+        elif event.type == pg.MOUSEMOTION:
+            if self.dragging:
+                mouse_x, mouse_y = pg.mouse.get_pos()
+                self.offset.x = mouse_x + self.clickstart_offset.x
+                self.offset.y = mouse_y + self.clickstart_offset.y
+        
+        elif event.type == pg.MOUSEBUTTONDOWN and event.button == 4:
+            self.scale_size     *= self.incriment_up
+            self.scale_velocity *= self.incriment_up
+            self.scale_distance *= self.incriment_up
+            
+        elif event.type == pg.MOUSEBUTTONDOWN and event.button == 5:
+            self.scale_size     *= self.incriment_down
+            self.scale_velocity *= self.incriment_down
+            self.scale_distance *= self.incriment_down
+    
+    def create_sprites(self):
+        #CREATE STARS FROM FILE
+        for i in BODIES["Stars"]:
+            i = BODIES["Stars"][i]
+            colour = (i["Colour"]["r"],i["Colour"]["g"],i["Colour"]["b"])
+            star(
+                self,
+                i["Name"],
+                i["Size"]/2,
+                i["Position"]["x"],
+                i["Position"]["y"],
+                colour
+            )
 
+        #CREATE ASTEROID BELT USING FILE DATA
+        """CODE HERE"""
+
+        #CREATE BACKGROUND STARS USING FILE DATA
+        """CODE HERE"""
+
+        #CREATE PLANETS & MOONS FROM FILE
+        orbiters = ["Planets", "Moons"]
+        for orbiter in orbiters:
+            for i in BODIES[orbiter]:
+                i = BODIES[orbiter][i]
+                colour = (i["Colour"]["r"],i["Colour"]["g"],i["Colour"]["b"])
+                if orbiter == "Moons":
+                    #MOVES MOONS SLIGHTLY FURTHER OUT AND REVERSES ORBIT DIRECTION
+                    i["Distance"] *= 10
+                    i["Velocity"] *= -1
+                satellite(
+                    self,
+                    i["Name"],
+                    i["Orbits"],
+                    i["Size"],
+                    i["Distance"],
+                    i["Velocity"],
+                    colour
+                )
+    
+    def get_body_by_name(self, name):
+        """
+        SEARCHES ALL SPRITES IN THE SPRITE GROUP
+        AND RETURNS THE SPRITE WITH THE GIVEN NAME
+        """
+        for sprite in self:
+            if sprite.name == name:
+                return sprite
+        return False
 
 class star(pg.sprite.Sprite):
-    def __init__(self, name, size, x, y, colour):
-        super().__init__(visible_sprites)
-        self.name   = name
-        self.size   = size  * SETTINGS["Scales"]["Size"]
-        self.x      = x
-        self.y      = y
-        self.colour = colour
+    def __init__(self, sprite_group, name, size, x, y, colour):
+        super().__init__(sprite_group)
+        self.sprite_group   = sprite_group
+        self.name           = name
+        self.base_size      = size
+        self.base_radius    = 0
+        self.base_velocity  = 0
+        self.size           = self.base_size * self.sprite_group.scale_size
+        self.radius         = 0
+        self.velocity       = 0
+        self.x              = x
+        self.y              = y
+        self.colour         = colour
+        self.surface        = pg.display.get_surface()
     
-    def update(self, WIN):
-        pg.draw.circle(WIN, self.colour, (self.x, self.y), self.size)
+    def update(self):
+        self.scale()
+        self.draw()
+    
+    def scale(self):
+        self.size       = self.base_size * self.sprite_group.scale_size
+        self.velocity   = self.base_velocity * self.sprite_group.scale_velocity
+        self.radius     = self.base_radius * self.sprite_group.scale_distance
+    
+    def draw(self):
+        pg.draw.circle(
+            self.surface,
+            self.colour,
+            (self.x + self.sprite_group.offset.x, self.y + self.sprite_group.offset.y),
+            self.size)
 
 class satellite(star):
-    def __init__(self, name, parent_name, size, distance, velocity, colour):
-        super().__init__(name, size, 0, 0, colour)
-        self.radius     = distance * SETTINGS["Scales"]["Distance"]
-        self.velocity   = velocity * SETTINGS["Scales"]["Velocity"]
-        self.parent     = get_body_by_name(parent_name) #GETS SPRITE OF BODY TO ORBIT
+    def __init__(self, sprite_group, name, parent_name, size, radius, velocity, colour):
+        super().__init__(sprite_group, name, size, 0, 0, colour)
+        self.base_radius    = radius
+        self.base_velocity  = velocity
+        self.radius         = self.base_radius * sprite_group.scale_distance
+        self.velocity       = self.base_velocity * sprite_group.scale_velocity
+        self.parent         = sprite_group.get_body_by_name(parent_name) #GETS SPRITE OF BODY TO ORBIT
         self.center_of_rotation_x = self.parent.x
         self.center_of_rotation_y = self.parent.y
         self.angle = radians(randrange(0,360))
         self.x = self.center_of_rotation_x + self.radius * cos(self.angle)
         self.y = self.center_of_rotation_y - self.radius * sin(self.angle)
     
-    def update(self, WIN):
+    def update(self):
         self.center_of_rotation_x = self.parent.x
         self.center_of_rotation_y = self.parent.y
-        self.x = self.center_of_rotation_x + self.radius * cos(self.angle)
-        self.y = self.center_of_rotation_y - self.radius * sin(self.angle)
-        
-        pg.draw.circle(WIN, self.colour, (self.x, self.y), self.size)
-        
+        self.x = (self.center_of_rotation_x + self.radius * cos(self.angle))
+        self.y = (self.center_of_rotation_y - self.radius * sin(self.angle))
+        self.scale()
+        self.draw()
         self.angle = self.angle + self.velocity
         self.x = self.x + self.radius * self.velocity * cos(self.angle + pi / 2)
         self.y = self.y - self.radius * self.velocity * sin(self.angle + pi / 2)
 
 
-def create_sprites():
-    #CREATE STARS FROM FILE
-    for i in BODIES["Stars"]:
-        i = BODIES["Stars"][i]
-        colour = (i["Colour"]["r"],i["Colour"]["g"],i["Colour"]["b"])
-        star(
-            i["Name"],
-            i["Size"],
-            i["Position"]["x"],
-            i["Position"]["y"],
-            colour
-        )
-
-    #CREATE ASTEROID BELT USING FILE DATA
-    """CODE HERE"""
-
-    #CREATE BACKGROUND STARS USING FILE DATA
-    """CODE HERE"""
-
-    #CREATE PLANETS & MOONS FROM FILE
-    orbiters = ["Planets", "Moons"]
-    for obiter in orbiters:
-        for i in BODIES[obiter]:
-            i = BODIES[obiter][i]
-            colour = (i["Colour"]["r"],i["Colour"]["g"],i["Colour"]["b"])
-            satellite(
-                i["Name"],
-                i["Orbits"],
-                i["Size"],
-                i["Distance"],
-                i["Velocity"],
-                colour
-            )
-
 def run():
-    create_sprites()
+    visible_sprites = MoveableCameraGroup(SETTINGS)
+    visible_sprites.create_sprites()
+    
     clock = pg.time.Clock()
     run = True
     while run:
         clock.tick(FPS)
         
         WIN.fill((0, 0, 0))
-        visible_sprites.update(WIN)
+        visible_sprites.update()
         pg.display.update()
         
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = False
+            visible_sprites.move_camera(event)
     quit()
 
 
